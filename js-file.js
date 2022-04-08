29d16d3c60651ae3f6ae7c62330dd95a9824afe5
Derek Nunes <dereknunes28@gmail.com>
2022-04-08 01:39:43 -0500

    some framework for module / factories

---
+++ js-file.js
@@ -0,0 +1,22 @@
+// Gameboard object module
+const gameboard = (() => {
+    let board = Array(9);
+    const displayBoard = (board) => {
+
+    }
+    return { displayBoard };
+})();
+
+// displayController object module
+const displayController = (() => {
+    
+
+})();
+
+// Factory function for Player
+const Player = (name, marker) => {
+
+
+};
+
+console.log(gameboard.board);
