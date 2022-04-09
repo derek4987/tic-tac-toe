db179a2e7a2190c1a9ebaaa7a7af1b0aa83f3f31
Derek Nunes <dereknunes28@gmail.com>
2022-04-08 22:52:41 -0500

    shell UI, gameBoard module updated

--- js-file.js
+++ js-file.js
@@ -1,10 +1,39 @@
 // Gameboard object module
 const gameboard = (() => {
-    let board = Array(9);
-    const displayBoard = (board) => {
-
+    let board = Array(9).fill('');
+    const displayBoard = () => {
+        const boardGrid = document.querySelector('#board');
+        boardGrid.innerHTML = `
+            <div class="square">
+                <div id="s0">${board[0]}</div>
+            </div>
+            <div class="square">
+                <div id="s1">${board[1]}</div>
+            </div>
+            <div class="square">
+                <div id="s2">${board[2]}</div>
+            </div>
+            <div class="square">
+                <div id="s3">${board[3]}</div>
+            </div>
+            <div class="square">
+                <div id="s4">${board[4]}</div>
+            </div>
+            <div class="square">
+                <div id="s5">${board[5]}</div>
+            </div>
+            <div class="square">
+                <div id="s6">${board[6]}</div>
+            </div>
+            <div class="square">
+                <div id="s7">${board[7]}</div>
+            </div>
+            <div class="square">
+                <div id="s8">${board[8]}</div>
+            </div>
+        `;
     }
-    return { displayBoard };
+    return { displayBoard, board };
 })();
 
 // displayController object module
@@ -18,5 +47,3 @@ const Player = (name, marker) => {
 
 
 };
-
-console.log(gameboard.board);
